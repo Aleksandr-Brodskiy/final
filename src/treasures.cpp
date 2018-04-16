ea455483a010b3c8111c74e027d3bb5a8b14148a
Christine Breckenridge <cbreckenridge@ucmerced.edu>
2018-04-16 15:21:36 -0700

    changed map key to string

--- src/treasures.cpp
+++ src/treasures.cpp
@@ -1,9 +1,20 @@
 #include <ros/ros.h>
 #include "std_msgs/String.h"
 #include "../../../devel/include/logical_camera_plugin/logicalImage.h"
+#include <string>
+
+std::map<std::string, logical_camera_plugin::logicalImage> treasures;
+int treasure_count = 0;
+
 void cameraMessageReceived(const logical_camera_plugin::logicalImage&msg) {
-	ROS_INFO_STREAM(msg);
+	treasures[msg.modelName] = msg;
+	if(treasure_count != treasures.size()) {//found a new treasure
+		treasure_count = treasures.size();
+		ROS_INFO_STREAM("Found a new treasure");
+		ROS_INFO_STREAM(msg);	
+	}
 }
+
 int main(int argc,char **argv) {
         ros::init(argc,argv,"treasures");
         ros::NodeHandle nh;
